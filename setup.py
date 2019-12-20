29ec552462106bd06da64d01e304b74bc5416606
Ivan Lee <ivanklee86@gmail.com>
2019-12-20 08:29:51 -0500

    Automatic version bump via bumpversion.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
 
 setup(
     name='flask_unleash',
-    version='1.0.2',
+    version='1.0.3',
     url='https://github.com/Unleash/Flask-Unleash',
     license='MIT',
     author='Ivan Lee',
