55b19646dd2eb2e0e12c76196fd40b3a96790d08
Ivan Lee <ivanklee86@gmail.com>
2019-12-20 05:12:59 -0500

    Add wsgi config to demo app.

---
+++ demo_app/gunicorn.py
@@ -0,0 +1,4 @@
+bind="0.0.0.0:5001"
+workers=4
+accesslog="-"
+worker_class = "gevent"
---
+++ demo_app/wsgi.py
@@ -0,0 +1,5 @@
+from app import create_app
+
+# Required by WSGI
+# pylint: disable=invalid-name
+application = create_app()
